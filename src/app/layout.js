4d4afd83c6ee2148fdb09e815d85cfa9f16f8898
Rohit Singh <rohitsingh@example.com>
2026-02-06 14:57:10 +0530

    start a new project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,16 @@
-// src/app/layout.js
-import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import { WishlistProvider } from "@/Context/WishlistContext";
-import  CartProvider from "@/Context/Cardcontext.jsx";
-import { Toaster } from "react-hot-toast";
-
-export const metadata = {
-  title: "EcoShop",
-  description: "Eco-Friendly E-Commerce Website",
-};
-
-export default function RootLayout({ children }) {
-  return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
+  import "./globals.css";
  
-<html lang="en">
-    <body className="antialiased">
-       
-        <CartProvider>
-            <WishlistProvider>{children}</WishlistProvider>
-        </CartProvider>
-        <Toaster position="top-right" reverseOrder={false} />
-    </body>
-</html>
-    </ClerkProvider>
+
+ 
+
+export default function RootLayout({
+  children,
+}) {
+  return (
+    <html lang="en">
+      <body>
+          {children}
+      </body>
+    </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,14 @@
-"use client"; // Only if you're using App Router (Next.js 13+)
+"use client"; 
 
-import { useState, useEffect } from "react";
-import Navbar from "@/components/Navbar";
-import Home from "@/components/home/Home";
-import Loder from "@/components/Loader";
-import Footer from "@/components/Footer";
+import { } from "react";
+ 
 
 export default function HomePage() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000); // Loader will show for 3 seconds
-
-    return () => clearTimeout(timer); // Clean-up
-  }, []);
+ 
 
   return (
     <>
-      {loading ? (
-        <Loder />
-      ) : (
-        <>
-          <Navbar />
-          <Home />
-          <Footer />
-        </>
-      )}
+    asfddgs
     </>
   );
 }
